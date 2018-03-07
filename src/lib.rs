c98eab349be79986b38c2412dc80800a487f123a
Adam Kelly <adamkelly2201@gmail.com>
2018-03-07 15:26:13 +0000

    Fix Benchmarks

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![crate_name = "qcgpu"]
 #![feature(test)]
 
 //! An Open Source, High Performance & GPU Accelerated, Quantum Computer Simulator in Rust
@@ -15,7 +14,7 @@ pub mod gates;
 #[cfg(test)]
 mod tests {
     use test::{Bencher, black_box};
-    use arrayfire::{set_backend, Backend};
+    use arrayfire::{set_backend, Dim4, DType, identity_t, Backend};
     use super::*;
 
     #[bench]
@@ -54,7 +53,7 @@ mod tests {
     fn qubits_8_hadamard_gate_cpu(b: &mut Bencher) {
         set_backend(Backend::CPU);
         b.iter(|| {
-            black_box(gates::generate_gate(&gates::hadamard(), 8, 3));
+            black_box(gates::generate_gate(gates::hadamard(), 8, 3));
         });
     }
 
@@ -62,7 +61,7 @@ mod tests {
     fn qubits_8_hadamard_gate_opencl(b: &mut Bencher) {
         set_backend(Backend::OPENCL);
         b.iter(|| {
-            black_box(gates::generate_gate(&gates::hadamard(), 8, 3));
+            black_box(gates::generate_gate(gates::hadamard(), 8, 3));
         });
     }
 
@@ -76,14 +75,15 @@ mod tests {
             black_box(kron::kron(&arr_1, &arr_2));
         });
     }
-     #[bench]
+
+    #[bench]
     fn kron_a_opencl(b: &mut Bencher) {
         set_backend(Backend::OPENCL);
         let arr_1 = identity_t(Dim4::new(&[4,4,1,1]), DType::C32);
         let arr_2 = identity_t(Dim4::new(&[2,2,1,1]), DType::C32);
 
         b.iter(|| {
-            black_box();
+            black_box(kron::kron(&arr_1, &arr_2));
         });
     }
 }
