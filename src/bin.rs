d5706a746f16b596beb7e48047c20193374a560c
Adam Kelly <adamkelly2201@gmail.com>
2018-03-10 23:51:19 +0000

    Better Bin Target

--- src/bin.rs
+++ src/bin.rs
@@ -1,7 +1,15 @@
 extern crate qcgpu;
+extern crate arrayfire;
 
 use qcgpu::state::QState;
+use qcgpu::gates::x;
+use arrayfire::print;
 
 fn main() {
-    let state = QState::from_bit_string("|001>");
+    let mut state = QState::from_bit_string("|00>");
+    print(&state.amplitude);
+    state.apply_gate(x(), 0);
+    state.cnot(0, 1);
+
+    print(&state.amplitude);
 }
