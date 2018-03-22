ec53c24ec8fd99d55459b7e2051fd84ccdecfed4
Adam Kelly <adamkelly2201@gmail.com>
2018-03-22 16:47:23 +0000

    Code Formatting

--- src/bin.rs
+++ src/bin.rs
@@ -4,8 +4,10 @@ use qcgpu::{Gate, State};
 
 fn main() {
     let x = Gate {
-        a: 0.0, b: 1.0,
-        c: 1.0, d: 0.0,
+        a: 0.0,
+        b: 1.0,
+        c: 1.0,
+        d: 0.0,
     };
 
     let mut state = State::new(5, 1);
--- src/lib.rs
+++ src/lib.rs
@@ -18,32 +18,28 @@ mod tests {
     #[bench]
     fn qubits_15_not_all_gpu(b: &mut Bencher) {
         let x = Gate {
-        a: 0.0,
-        b: 1.0,
-        c: 1.0,
-        d: 0.0,
-    };
+            a: 0.0,
+            b: 1.0,
+            c: 1.0,
+            d: 0.0,
+        };
         b.iter(|| {
             let mut state = State::new(15, 1);
-            for i in 0..15 {
-                black_box(state.apply_gate(i, x));
-            }
+            black_box(state.apply_all(x));
         });
     }
 
     #[bench]
     fn qubits_15_not_all_cpu(b: &mut Bencher) {
         let x = Gate {
-        a: 0.0,
-        b: 1.0,
-        c: 1.0,
-        d: 0.0,
-    };
+            a: 0.0,
+            b: 1.0,
+            c: 1.0,
+            d: 0.0,
+        };
         b.iter(|| {
             let mut state = State::new(15, 0);
-            for i in 0..15 {
-                black_box(state.apply_gate(i, x));
-            }
+            black_box(state.apply_all(x));
         });
     }
 }
