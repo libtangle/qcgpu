68676b9c70844a2887509ba800066a81fdf90b33
Adam Kelly <adamkelly2201@gmail.com>
2018-03-23 19:13:51 +0000

    Add Gates

--- src/gates.rs
+++ src/gates.rs
@@ -1,6 +1,29 @@
+//! Gates and Gate Generation
+//!
+//! Matrices are in row major format, and
+//! all gates use the num_complex::Complex&lt;f32&gt;
+//! datatype.
+
 use num_complex::Complex32;
 use std::fmt;
+use std::f32::consts::FRAC_1_SQRT_2;
 
+
+/// Representation of a gate
+///
+/// # Examples
+///
+/// ```
+///# extern crate qcgpu;
+///# extern crate num_complex;
+///# use qcgpu::Gate;
+///# use num_complex::Complex32;
+/// Gate {
+///    a: Complex32::new(0.0, 0.0), b: Complex32::new(1.0, 0.0),
+///    c: Complex32::new(1.0, 0.0), d: Complex32::new(0.0, 0.0)
+/// };
+///
+///
 #[derive(Debug, Clone, Copy)]
 pub struct Gate {
     pub a: Complex32,
@@ -10,7 +33,115 @@ pub struct Gate {
 }
 
 impl fmt::Display for Gate {
-     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-         write!(f, "[[{}, {}], [{}, {}]]", self.a, self.b, self.c, self.d)
-     }
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[[{}, {}], [{}, {}]]", self.a, self.b, self.c, self.d)
+    }
+}
+
+/// Identity gate
+///
+/// [1, 0]
+///
+/// [0, 1]
+pub fn id() -> Gate {
+    Gate {
+        a: Complex32::new(1.0, 0.0),
+        b: Complex32::new(0.0, 0.0),
+        c: Complex32::new(0.0, 0.0),
+        d: Complex32::new(1.0, 0.0),
+    }
+}
+
+/// Hadamard Gate
+///
+/// [0.70710678118, 0.70710678118]
+///
+/// [0.70710678118, -0.70710678118]
+#[inline]
+pub fn h() -> Gate {
+    Gate {
+        a: Complex32::new(FRAC_1_SQRT_2, 0.0),
+        b: Complex32::new(FRAC_1_SQRT_2, 0.0),
+        c: Complex32::new(FRAC_1_SQRT_2, 0.0),
+        d: Complex32::new(-FRAC_1_SQRT_2, 0.0),
+    }
+}
+
+/// Pauli X / NOT Gate
+///
+/// [0, 1]
+///
+/// [1, 0]
+#[inline]
+pub fn x() -> Gate {
+    Gate {
+        a: Complex32::new(0.0, 0.0),
+        b: Complex32::new(1.0, 0.0),
+        c: Complex32::new(1.0, 0.0),
+        d: Complex32::new(0.0, 0.0),
+    }
+}
+
+/// Pauli Y Gate
+///
+/// [0, -i]
+///
+/// [i, 0]
+#[inline]
+pub fn y() -> Gate {
+    Gate {
+        a: Complex32::new(0.0, 0.0),
+        b: Complex32::new(0.0, -1.0),
+        c: Complex32::new(0.0, 1.0),
+        d: Complex32::new(0.0, 0.0),
+    }
+}
+
+/// Pauli Z Gate
+///
+/// [1, 0]
+///
+/// [0, -1]
+#[inline]
+pub fn z() -> Gate {
+    Gate {
+        a: Complex32::new(1.0, 0.0),
+        b: Complex32::new(0.0, 0.0),
+        c: Complex32::new(0.0, 0.0),
+        d: Complex32::new(-1.0, 0.0),
+    }
+}
+
+/// S / Phase Gate
+///
+/// [1, 0]
+///
+/// [0, i]
+#[inline]
+pub fn s() -> Gate {
+    Gate {
+        a: Complex32::new(1.0, 0.0),
+        b: Complex32::new(0.0, 0.0),
+        c: Complex32::new(0.0, 0.0),
+        d: Complex32::new(0.0, 1.0),
+    }
+}
+
+
+/// T Gate
+///
+/// [1, 0]
+///
+/// [0,(1/sqrt(2)) * 1 + 1i]
+#[inline]
+pub fn t() -> Gate {
+    Gate {
+        a: Complex32::new(1.0, 0.0),
+        b: Complex32::new(0.0, 0.0),
+        c: Complex32::new(0.0, 0.0),
+        d: Complex32::new(
+            0.7071067811865475244008443621048490393,
+            0.7071067811865475244008443621048490393,
+        ),
+    }
 }
