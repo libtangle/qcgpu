38fd901e03721414780655db8deaa2ef002c0a78
Adam Kelly <adamkelly2201@gmail.com>
2018-03-23 19:46:29 +0000

    Add GHZ State Preperation Example

---
+++ examples/ghz-state.rs
@@ -0,0 +1,32 @@
+//! Preperation of the 3 qubit GHZ state
+
+extern crate qcgpu;
+
+use qcgpu::{State};
+use qcgpu::gates::{x, h};
+
+fn main() {
+    // New Quantum State with 3 qubits
+    let mut state = State::new(3, 1);
+
+    // Print the hardware that the simulation will run on
+    print!("Running On: ");
+    state.info();
+
+    // Apply the gates
+    state.apply_gate(0, h());
+    state.apply_gate(1, h());
+    state.apply_gate(2, x());
+
+    state.apply_controlled_gate(1, 2, x());
+    state.apply_controlled_gate(0, 2, x());
+
+    state.apply_all(h());
+
+    // XXX Measurement
+    state.apply_all(h());
+
+    println!("State Vector: {}", state);
+    println!("Probabilities: {:?}", state.get_probabilities());
+    println!("Measured: {}", state.measure());
+}
