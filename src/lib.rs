00e2eef812af5f787944eee52364345cecde4874
Adam Kelly <adamkelly2201@gmail.com>
2018-04-01 22:41:38 +0100

    Update lib.rs documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,24 @@
-//#![warn(missing-debug-implementations, missing-docs, trivial-casts, trivial-numeric-casts, unused-extern-crates, unused-import-braces, unused-qualifications, unused-results)]
-
 #![deny(missing_debug_implementations, missing_docs, trivial_casts, trivial_numeric_casts,
         unused_extern_crates, unused_import_braces, unused_qualifications, unused_results)]
+
 //! An Open Source, High Performance & Hardware Accelerated, Quantum Computer Simulator in Rust
 //!
-//! See the code [here](https://github.com/QCGPU/QCGPU-rust).
+//! This crate is a library with the aim to provide fast and effecient simulation of algorithms
+//! for quantum computers, while also being easy to use and abstracting away the management of the
+//! OpenCL Devices
 //!
-//! Decoherence can be enabled as a feature `decoherence`. The amount of decoherence can then be
-//! set through the method `set_decoherence`
+//! The best place to start with this library is with the [user guide](https://qcgpu.github.io/qcgpu/book),
+//! along with the docs (you're looking at them) and the [source code](https://github.com/QCGPU/QCGPU-rust)
+//!
+//! ## Features
+//! * Simulation of arbitrary quantum algorithms
+//! * Optional simulation of decoherence
+//! * Optimized for maximally entangled states
+//! * Accelerated with GPUs, FPGAs and other OpenCL devices
+//! * Example implementations of Grover, Deutsch-Jozsa, Bernstein-Vazirani and Shors algorithm
+//! * Implements Hadamard, Pauli and phase gates, with support for arbitrary gates
+//! * Support for arbitrary controlled gates
+
 
 extern crate num_complex;
 extern crate ocl;
