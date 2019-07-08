52d699834c8af9876d8947298452f30a07ee999c
Adam Kelly <adam.guitar12@gmail.com>
2019-07-09 00:40:53 +0400

    Cleaned up CMakeLists

--- example.c
+++ example.c
@@ -1,14 +1,14 @@
 #include "tangle.h"
 
 #include <stdio.h>
-#include "mpi.h"
+// #include "mpi.h"
 
 int main() {
-  MPI_Init(NULL, NULL);
+  // MPI_Init(NULL, NULL);
 
   count_to(10); 
 
-  MPI_Finalize();
+  // MPI_Finalize();
 
   return 0;
 }
