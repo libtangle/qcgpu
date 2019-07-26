a96845b0f2f444ab42c4a48912b630cf564ce6ca
Adam Kelly <adamkelly2201@gmail.com>
2019-07-27 00:21:19 +0100

    Used C++ to integrate with Cython

---
+++ src/library.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+namespace Tangle
+{
+
+void say_hello()
+{
+    std::cout << "Pretty Tangled Here" << std::endl;
+}
+
+} // namespace Tangle
--- tangle/_tangle.pyx
+++ tangle/_tangle.pyx
@@ -1,2 +1,5 @@
-cpdef say_hi ():
-    print("Hello World")
+cdef extern from "library.cpp" namespace "Tangle":
+    cdef void say_hello()
+
+def say_hi():
+    return say_hello()
