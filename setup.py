5804b8e9b96ff75397ac7d6ca1bf2aeeed6f32aa
Adam Kelly <adamkelly2201@gmail.com>
2019-07-26 23:35:53 +0100

    Wrote Setup Information

--- setup.py
+++ setup.py
@@ -1,7 +1,30 @@
 from skbuild import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='tangle',
-    version='0.2.0',
-    packages=['tangle']
+    name="tangle",
+    version="1.0.0",
+    author="Adam Kelly",
+    author_email="adamkelly2201@gmail.com",
+    description="High Performance Tools for Quantum Computation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://libtangle.com",
+    packages=['tangle'],
+    classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: C++",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering",
+    ],
+    install_requires=['scikit-build']
 )
